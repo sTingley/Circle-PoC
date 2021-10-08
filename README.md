d9be3c13a84194ef01eb368729966a494f5e1617
Samuel Stokes <sam.stokes@mesh.xyz>
2021-10-08 09:42:02 -0400

    Use ACH for payouts.feature

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Circle-PoC
+
+## Constraints
+
+- Sandbox environment: wire payout takes ~25min to complete
+- Sandbox environment: ACH payout takes 16-22min to complete (but gets subtracted from balance within ~1min)
+- For both payments and payouts, there is a per-transaction limit of $25,000.
+- Learn about settlement times and cut-offs to better understand when funds from payments will settle and when payouts will reach customer bank accounts.
--- features/step_definitions/payouts.js
+++ features/step_definitions/payouts.js
@@ -1,37 +1,62 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
-const {
-  getMasterWalletId,
-  getMasterWalletBalance,
-  getCustomerWireAccounts,
-  sendWirePayout
-} = require('../../src/server.js');
+const { getMasterWalletBalance, getCustomerACHAccounts, sendACHPayout } = require('../../src/server.js');
 
 let originalTotalBalance;
 let bankAccounts;
 let payouts = [];
 
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 Given("{int} Investor Bank Accounts are linked to END's Circle Account", async (numAccounts) => {
-  bankAccounts = await getCustomerWireAccounts();
+  bankAccounts = await getCustomerACHAccounts();
+  console.log('Hello..................');
   assert(bankAccounts.length >= numAccounts);
 });
 
 Given('we know the initial END Circle Account balance', async () => {
-  const masterBalance = await getMasterWalletBalance();
+  const currentBalance = await getMasterWalletBalance();
   originalTotalBalance =
-    Number(masterBalance.available[0].amount) + Number(masterBalance.unsettled[0].amount);
-  assert(masterBalance.available[0].amount.length > 0);
+    Number(currentBalance.available[0].amount) + Number(currentBalance.unsettled[0].amount);
+  assert(currentBalance.available[0].amount.length > 0);
 });
 
 When(
-  'END initiates a ${int} payment to each of the {int} Investor Bank Accounts',
+  'END initiates a ${int} payout to each of the {int} Investor Bank Accounts',
   async (amount, numAccounts) => {
     for (let i = 0; i < numAccounts; i++) {
-      payouts[i] = await sendWirePayout(amount, bankAccounts[0].id);
+      payouts[i] = await sendACHPayout(amount, bankAccounts[0].id);
     }
   }
 );
 
-Then('the END Circle Account balance should decrease by ${int}', function (int) {
-  return 'pending';
-});
+Then(
+  'the END Circle Account balance should be ${int} less than the initial balance',
+  { timeout: 30 * 1000 },
+  async (amount) => {
+    // Give payouts time to be deducted from balance
+    let payoutsComplete = false;
+    let attempts = 0;
+    let newTotalBalance;
+    while (!payoutsComplete) {
+      attempts++;
+      console.log(`Polling account balance. Attempt #${attempts}`);
+      const currentBalance = await getMasterWalletBalance();
+      newTotalBalance =
+        Number(currentBalance.available[0].amount) + Number(currentBalance.unsettled[0].amount);
+      console.log('originalTotalBalance:', originalTotalBalance);
+      console.log('newTotalBalance:', newTotalBalance);
+
+      payoutsComplete = true;
+      if (newTotalBalance === originalTotalBalance) {
+        payoutsComplete = false;
+      }
+      // Wait 3 seconds before polling again
+      console.log(`Waiting 3 sec before trying again`);
+      await sleep(3000);
+    }
+    assert(newTotalBalance === originalTotalBalance - amount);
+  }
+);
