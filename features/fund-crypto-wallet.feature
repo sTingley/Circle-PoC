359cbf7b3255ad4570df21b3f7557d9a6019f20c
Spencer Tingley <tings@protonmail.com>
2021-10-08 10:21:50 -0400

    fix fund crypto .feature file and tests syntax

--- features/fund-crypto-wallet.feature
+++ features/fund-crypto-wallet.feature
@@ -1,11 +1,11 @@
 Feature: END can move funds into a blockchain address provided within Circle
 	As END
-	I want to move funds from an external blockchain address to a Circle blockchain address
-	So that I can make future payouts to customers
+	I want be able to receive funds from an external blockchain address to a Circle blockchain address
+	So that I can make payouts from the Circle blockchain addresses
 
 	Scenario: Circle Master Account receives payment in crypto
-    Given END's Circle Account has a blockchain wallet created
-		And END's Circle blockchain wallet has a deposit address
-    And we know the initial END Circle blockchain account balance
-    When END receives a $333 payment in crypto
-    Then the END Circle Account crypto account balance should increase by $333 in crypto
+    Given ENDs Circle Account has a blockchain wallet created
+		And ENDs Circle blockchain wallet has a deposit address
+    And The initial END Circle blockchain account balance is known
+    When END receives a $1 payment of USDC
+    Then ENDs Circle Account crypto account balance should increase by $1
--- features/step_definitions/fund-crypto-wallet.js
+++ features/step_definitions/fund-crypto-wallet.js
@@ -1,28 +1,33 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
 const {
-  createCryptoWallet
+  createCryptoWallet, generateDepositAddress
 } = require('../../src/server.js');
 
-const cryptoWallet;
-const depositAddress; //Could be many later
+let cryptoWallet;
+let depositAddress; //Could be many addresses later
 
-Given('I have a valid Circle blockchain wallet', async () => {
+Given('ENDs Circle Account has a blockchain wallet created', async () => {
   cryptoWallet = await createCryptoWallet("testSuite");
   assert(cryptoWallet.walletId.length > 0);
 });
 
-Given('I have a deposit address for my Circle blockchain wallet', async () => {
+Given('ENDs Circle blockchain wallet has a deposit address', async () => {
   depositAddress = await generateDepositAddress(cryptoWallet.walletId, "USD", "ETH");
 	assert(depositAddress.address.length > 0);
 	console.log("deposit address: " + depositAddress.address);
 });
 
-When('I receive ${int} external funds into my Circle address (wallet)', async (amount) => {
-	//initiate trxn of token transfer of USDC (Goerli network) or ETH
-	//Web3 token transfer
+Given('The initial END Circle blockchain account balance is known', async () => {
+  return true;
 });
 
-Then('My balance should increase by ${int}', function () {
+When('END receives a ${int} payment of USDC', async (amount) => {
+	//initiate trxn of token transfer of USDC (Goerli network) or ETH
+	//Web3 token transfer
+  return true;
+});
+
+Then('ENDs Circle Account crypto account balance should increase by ${int}', function (amount) {
   return true;
 });
